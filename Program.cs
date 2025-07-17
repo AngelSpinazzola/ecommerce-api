928488c4f9ad612a67fe4d899b87b8ad1f8f6581
AngelSpinazzola <angel_spinazzola@outlook.es>
2025-07-17 02:27:49 -0300

    Agregando inyección de dependencia

--- Program.cs
+++ Program.cs
@@ -94,6 +94,8 @@ builder.Services.AddScoped<IFileService, FileService>();
 builder.Services.AddScoped<IProductImageService, ProductImageService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
+builder.Services.AddScoped<ICheckoutService, CheckoutService>();
+builder.Services.AddHttpClient<IMercadoPagoService, MercadoPagoService>();
 
 var app = builder.Build();
 
